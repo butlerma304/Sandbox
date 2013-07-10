21e3a2690c3cf163c8d0043ab2a8a15371d682c8
Marcus Butler <butlerma304@gmail.com>
2013-07-10 13:43:13 -0400

    Refractored Package Locations
    Sandbox DMGPropertyService as Root

--- DmgPropertyService/DmgPropertyService.Data/AutoMapperDataConfiguration.cs
+++ DmgPropertyService/DmgPropertyService.Data/AutoMapperDataConfiguration.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ClassLibrary1
+namespace DmgPropertyService.Data
 {
-    public class Class1
+    public class AutoMapperDataConfiguration
     {
     }
 }
--- DmgPropertyService/DmgPropertyService.Domain/AutoMapperDomainConfiguration.cs
+++ DmgPropertyService/DmgPropertyService.Domain/AutoMapperDomainConfiguration.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DmgPropertyService.Data
+namespace DmgPropertyService.Domain
 {
-    public class c
+    class AutoMapperDomainConfiguration
     {
     }
 }
